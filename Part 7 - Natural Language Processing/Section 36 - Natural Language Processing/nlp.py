52ba6aadeff8fb9f3370f95a69c1448ef820bb84
G Naveen <naveengondu18@gmail.com>
2020-03-27 20:22:52 +0530

    added Classifier

--- Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/nlp.py
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/nlp.py
@@ -19,3 +19,20 @@ for i in range(1000):
     review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
     review = ' '.join(review)
     corpus.append(review)
+    
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
